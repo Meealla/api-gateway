0a24663f090cd83a20ea4345185069b06757e1dc
Анастасия <nastenka.grigoreva@gmail.com>
2025-01-07 12:03:06 +0500

    Исправлен порт api-gateway. Исправлен код стайл в тестах.

--- api-gateway/src/test/java/ru/javabegin/micro/planner/apigateway/infrastructure/controller/ApiGatewayApplicationTest.java
+++ api-gateway/src/test/java/ru/javabegin/micro/planner/apigateway/infrastructure/controller/ApiGatewayApplicationTest.java
@@ -1,5 +1,6 @@
 package ru.javabegin.micro.planner.apigateway.infrastructure.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,22 +9,23 @@ import webapp.apigateway.ApiGatewayApplication;
 
 @SpringBootTest(classes = ApiGatewayApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApiGatewayApplicationTest {
+    @Autowired
+    private WebTestClient webTestClient;
 
-  @Autowired
-  private WebTestClient webTestClient;
-
-  @Test
-  public void testAnalyzerRoute() {
-    webTestClient.get().uri("/analyzer/hobbies")
+    @Test
+    @DisplayName("Тест для конечной точки анализатора")
+    public void testAnalyzerRoute() {
+        webTestClient.get().uri("/analyzer/hobbies")
       .exchange()
       .expectStatus().isOk();
-  }
+    }
 
-  @Test
-  public void testGeneratorRoute() {
-    webTestClient.get().uri("/generator/templates")
+    @Test
+    @DisplayName("Тест для конечной точки генератора")
+    public void testGeneratorRoute() {
+        webTestClient.get().uri("/generator/templates")
       .exchange()
       .expectStatus().isOk();
-  }
+    }
 }
 
